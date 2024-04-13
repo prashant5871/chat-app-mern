6b6c58c1ee7deae79f2b9d982267f08ecb3c65c3
Prashant Kalsariya <chhabilbhaikalsariya@gmail.com>
2024-04-13 11:30:06 +0530

    react+vite created and basic routing + css configration done

---
+++ frontend/src/App.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react'
+import {createBrowserRouter,RouterProvider} from "react-router-dom";
+import './App.css'
+import HomePage from './components/HomePage';
+import Register from './components/Register';
+import Login from './components/Login';
+
+const router = createBrowserRouter(
+  [
+    {
+      path:"/",
+      element:<HomePage/>
+    },
+    {
+      path:"/register",
+      element:<Register/>
+    },
+    {
+      path:"/login",
+      element:<Login/>
+    }
+  ]
+)
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className="App text-white p-4 h-screen flex items-center justify-center">
+        <RouterProvider router={router}/>
+      </div>
+    </>
+  )
+}
+
+export default App
